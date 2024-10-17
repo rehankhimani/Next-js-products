1367583a1513b4ad7e4dab0f26839f5b441d97bd
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2024-10-17 20:33:12 +0500

    Add project

---
+++ git project/helloworld/app/hello.tsx
@@ -0,0 +1,8 @@
+export default function First (){
+    return (
+        <div>
+            <h1>hello World</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam cumque sapiente totam eos harum, quaerat, dolorum quam error maiores, fugit voluptate vitae officia enim molestias! Dignissimos saepe iure nesciunt inventore.</p>
+            </div>
+    )
+}
---
+++ git project/helloworld/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function Home (){
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tenetur quis delectus aspernatur quos quia, ex mollitia, sapiente eum facere dolores magnam ipsam, repudiandae nulla. Soluta inventore eligendi similique eaque.</p>
+    </div>
+  )
+}
